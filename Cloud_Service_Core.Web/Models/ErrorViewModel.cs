59f420c36e4e0d86f37818d46ee591bdca023b9a
Ivan Beschetnikov <ivanbesch@outlook.com>
2018-06-24 13:14:40 +0300

    Initial commit

---
+++ Cloud_Service_Core.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cloud_Service_Core.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
